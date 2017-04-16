74004031b2ff073c131f8a56d3910e964720b240
kkawalec <aedriell@gmail.com>
2017-04-16 13:39:35 +0200

    creating simple table template, enable scss

--- src/app/cats/cats-table.component.ts
+++ src/app/cats/cats-table.component.ts
@@ -2,9 +2,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: `
-    <h2>Cats</h2>
-    <p>List of cats</p>`
+  templateUrl: './cats-table.component.html',
 })
 
 // Component class
